5d25539f5b3283a2d7b6edcc6eba015fdc1e4c4c
Emilio Garcia <github@emiliogq.com>
2022-12-27 18:36:01 +0100

    Add initial plotting

---
+++ src/data_analysis.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+from genetic_algorithm import GeneticAlgorithm
+
+best_individual, max_fitnesses, avg_fitnesses = GeneticAlgorithm(100).run(200, 0.9, 0.1, 50)
+
+plt.plot(max_fitnesses, color="red")
+plt.plot(avg_fitnesses, color="green")
+plt.xlabel("Generations")
+plt.ylabel("Fitness over iterations")
+plt.legend(["Max fitness", "Average fitness"])
+plt.show()
